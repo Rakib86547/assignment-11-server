8b4a3b0e68c0bc6c479731d8b881498de1e0fe36
Rakib86547 <contact.antor.bd@gmail.com>
2022-11-08 20:36:50 +0600

    services api create

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middle ware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.tiiizrg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try {
+        const serviceCollection = client.db('albertLawyer').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
+
 
 app.get('/', (req, res) => {
     res.send('api running')

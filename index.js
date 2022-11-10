6d731b9c1358f60691f003208aaad94d7d0143d5
Rakib86547 <contact.antor.bd@gmail.com>
2022-11-10 13:11:05 +0600

    last commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { query } = require('express');
 const app = express();
@@ -14,6 +15,21 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.tiiizrg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT (req, res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+        return res.send({message: 'unauthorize access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded) {
+        if(err) {
+            return res.send({message: 'unauthorize access'});
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
 
 async function run() {
     try {
@@ -32,6 +48,12 @@ async function run() {
             res.send(services);
         });
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '1h'});
+            res.send({token});
+        })
+
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
@@ -45,6 +67,7 @@ async function run() {
             });
 
             app.get('/reviews', async (req, res) => {
+                
                 let query = {};
                 const email = req.query.email;
                 if (email) {
@@ -52,7 +75,6 @@ async function run() {
                         email: email
                     }
                 };
-                console.log(email, query)
                 const cursor = reviewsCollection.find(query);
                 const result = await cursor.toArray();
                 res.send(result)
